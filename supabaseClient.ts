49e40818fa25253562991a2978d04e03ff3c9824
Ansteikoku <tianzhongxiaoji75@gmail.com>
2025-12-30 00:14:47 +0900

    Update supabaseClient.ts

--- supabaseClient.ts
+++ supabaseClient.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
 export const supabase = createClient(
-  'https://YOUR_PROJECT_ID.supabase.co',
-  'YOUR_PUBLIC_ANON_KEY'
+  'https://furhathilthcnoxfdmzv.supabase.co',
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZ1cmhhdGhpbHRoY25veGZkbXp2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjcwMTQyMzMsImV4cCI6MjA4MjU5MDIzM30.5niAFnw7GqtVQVSMdp6LuRZnALu8C8Gyjdodp2FOpgY'
 );
